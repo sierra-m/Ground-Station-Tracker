9daa8f5d1be64dcc106d6bc839b9eca53fb5d71a
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-07 00:10:12 -0500

    balloon coord: updatee api to use new borealis endpoints

--- Balloon_Coordinates.py
+++ Balloon_Coordinates.py
@@ -25,97 +25,157 @@ from calendar import timegm
 import requests
 import time
 import sys
+import re
+
+class Modem:
+    def __init__(self, **kwargs):
+        self.partial_imei = kwargs.get('partialImei', '')
+        self.org = kwargs.get('org', '')
+        self.name = kwargs.get('name', '')
+
+    def list_name(self):
+        return f'({self.partial_imei}) {self.name}'
+
+    def __repr__(self):
+        return f'Modem:{{{self.list_name()}, org: {self.org}}}'
+
+
+class FlightPoint:
+    def __init__(self, **kwargs):
+        self.uid = kwargs['uid']
+        self.timestamp = kwargs['datetime']
+        self.latitude = kwargs.get('latitude', 0)
+        self.longitude = kwargs.get('longitude', 0)
+        self.altitude = kwargs.get('altitude', 0)
+        self.vertical_velocity = kwargs.get('vertical_velocity', 0)
+        self.ground_speed = kwargs.get('ground_speed', 0)
+        self.satellites = kwargs.get('satellites', 0)
+        self.input_pins = kwargs.get('input_pins', 0)
+        self.output_pins = kwargs.get('output_pins', 0)
+    
+    def get_coor_alt(self):
+        return [self.latitude, self.longitude, self.altitude]
 
 
 class Balloon_Coordinates:
-    BOREALIS_EPOCH = 1357023600
-
-    def __init__(self, imei):
-
-        self.imei = imei
+    def __init__(self):
+        self.modem_list = self.list_modems()
+        self.uid = None
+        self.chosen_modem = None
+        self.last_flight_date = None
+        self.last_data_point = None
+    
+    def select_modem(self, modem_name):
+        match = re.match(r'\(\d+\) (\w+)', modem_name)
+        if match:
+            modem_name = match.group(1)
+        if modem_name not in self.modem_list:
+            raise Exception(f'{modem_name} is not a valid modem')
+        
+        self.chosen_modem = self.modem_list[modem_name]
 
         try:
-            # Grab and Define IMEI's Latest Flight
-            req = requests.get("https://borealis.rci.montana.edu/meta/flights?imei={}".format(self.imei))
+            # Grab and Define modem's Latest Flight
+            req = requests.get(f"https://borealis.rci.montana.edu/api/meta/flights?modem_name={modem_name}")
         except requests.exceptions.RequestException:  # does not catch if there is no internet
             print("No internet connection detected")
             sys.exit(-1)
+        
+        last_flight = req.json()[-1]
 
-        self.latest_flight = req.json()[-1]
+        self.last_flight_date = last_flight['date']
+        self.uid = last_flight['uid']
 
-        # Define UID
-        flightTime = timegm(time.strptime(self.latest_flight, "%Y-%m-%d")) - Balloon_Coordinates.BOREALIS_EPOCH
-        self.uid = (int(flightTime) << 24) | int(self.imei[8:])
+        # Request points since an hour ago
+        point = self.get_update(int(time.time()) - 3600)
 
-        return
+        # Shouldn't happen, but just in case
+        if not point:
+            print("Flight point request error, check flight is active")
+            sys.exit(-1)
+        
+        self.last_data_point = point
+    
+    # Lightweight update endpoint to get last point
+    def get_update(self, since: int):
+        body = {
+            "uid": self.uid,
+            "datetime": since
+        }
 
+        try:
+            # Grab and Define modem's Latest Flight
+            req = requests.post(f"https://borealis.rci.montana.edu/api/update", json=body)
+        except requests.exceptions.RequestException:  # does not catch if there is no internet
+            print("No internet connection detected")
+            sys.exit(-1)
+        
+        data = req.json()
+
+        if not data['update']:
+            return None
+        
+        point = data['result'][-1]
+
+        return FlightPoint(**point)
 
     @staticmethod
-    def list_IMEI():
-    # grab the list of all of the imei's on the borealis server
+    def list_modems():
+    # grab the list of all of the modems on the borealis server
 
-        # Request IMEI List
+        # Request modem List
         try:
-            req = requests.get('https://borealis.rci.montana.edu/meta/imeis')
+            req = requests.get('https://borealis.rci.montana.edu/api/meta/modems')
         except requests.exceptions.RequestException:
             print("couldn't connect to internet")
             print("Please connect to internet and relaunch")
             sys.exit(-1)
 
         data = req.json()
-        IMEIs = []
 
-        for imei in data:
-            IMEIs.append(imei)
-        return IMEIs
+        return {item['name']: Modem(**item) for item in data}
+    
+    def get_modem_titles(self):
+        return [modem.list_name() for modem in self.modem_list.values()]
 
     def get_coor_alt(self):
-        # returns a list containing the lat, long, and alt of the latest ping from the selected IMEI
-        try:
-            req = requests.get("https://borealis.rci.montana.edu/flight?uid={}".format(self.uid))
-            print("https://borealis.rci.montana.edu/flight?uid={}".format(self.uid))
-        except requests.exceptions.RequestException:
-            print("couldn't get updated position (no internet probably)")
-            return []
+        point = self.get_update(since=self.last_data_point.timestamp)
+        if point:
+            self.last_data_point = point
+        coor_alt = self.last_data_point.get_coor_alt()
+        print(coor_alt)
 
-        data = req.json()
-        # print(data) # for debugging server problem, will print a lot
-
-        # print(data['data'][-1][3])
-
-        # Lat, Long, Alt
-        self.coor_alt = [data['data'][-1][3], data['data'][-1][4], data['data'][-1][5]]
-        print(self.coor_alt)
-
-        return self.coor_alt  # [lat, long, altitude]
+        return coor_alt  # [lat, long, altitude]
 
     def print_info(self):
         # prints the latest lat, long and alt of the balloon
         self.get_coor_alt()
-        print("IMEI: ", self.imei)
-        print("Date:", self.latest_flight)
-        print("Coordinates: (", self.coor_alt[0], ", ", self.coor_alt[1], ")")
-        print("Altitude: ", self.coor_alt[2])
+        lat = self.last_data_point.latitude
+        lng = self.last_data_point.longitude
+        alt = self.last_data_point.altitude
+        print(f"Modem: {self.chosen_modem}")
+        print(f"Date: {self.last_flight_date}")
+        print(f"Coordinates: ({lat}, {lng})")
+        print(f"Altitude: {alt}")
 
-        infoStr = "IMEI: " + self.imei + " Date: " + self.latest_flight
-        infoStr += "\n" + "Coordinates: (" + str(self.coor_alt[0]) + ", " + str(self.coor_alt[1]) + ")" + " Altitude: " + str(self.coor_alt[2])
-        infoStr += "\n Balloon Selected!"
-        return infoStr
+        return (f"Modem: {self.chosen_modem}, Date: {self.last_flight_date}\n"
+                f"Coordinates: ({lat}, {lng}), Altitude: {alt}\n"
+                "Balloon Selected!")
 
     def getTimeDiff(self):
         # finds the difference in time between the latest ping and the one before
-        req = requests.get("https://borealis.rci.montana.edu/flight?uid={}".format(self.uid))
-        data = req.json()
+        new_point = self.get_update(since=self.last_data_point.timestamp)
+        
+        if not new_point:
+            return 0
 
-        lastTime = [data['data'][-1][2], data['data'][-2][2]]
+        print(f'current ts: {new_point.timestamp}')
 
-        print(lastTime[0])
+        diff = new_point.timestamp - self.last_data_point.timestamp
+        print(f'diff: {diff}')
 
-        print(lastTime[0] - lastTime[1])
-        # return lastTime[0] - lastTime[1]
-        return lastTime[0] - lastTime[1]
-
-    pass
+        self.last_data_point = new_point
+        return diff
 
 
 # import requests
